31e53d8c91637ec5769be77248059cdaebeb333b
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-19 01:34:29 +0530

    simple main setup

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Hello world")
+}
+
+const (
+	QueueHackathonRegistrations = "ai-hackathon-registrations"
+	QueueWocRegistrations       = "woc-registrations"
+	QueueDebateRegistrations    = "debate-registrations"
+)
+
+var allQueues = []string{
+	QueueDebateRegistrations,
+	QueueWocRegistrations,
+	QueueHackathonRegistrations,
+}
+
+var Rabbit *MsgBroker
+
+type MsgBroker struct {
+	conn *amqp.Connection
 }
